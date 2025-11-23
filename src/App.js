542c0b21a58015d31028e3f599f9463bb5829d3f
Sumanth Reddy <sumanthreddy@Sumanths-MacBook-Air.local>
2025-11-24 00:04:12 +0530

    chore: add styles and markup updates for wishlist UI (App.css, App.js)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onToInputChange=(e)=>{
+    setTodo(e.target.value);
+  }
+
+  const addTodo=(e)=>{
+    e.preventDefault();
+    setTodos([...todos, {id: uuid(), todo: todo,isCompleted:false}]);
+    setTodo("");
+  }
+
+  const onTodoCheckChange=(id)=>{
+    const updatedTodos=todos.map((item)=>{
+      if(item.id===id){
+        return {...item, isCompleted: !item.isCompleted};
+      }
+      return item;
+    });
+    setTodos(updatedTodos);
+  }
+
+  const deleteTodo=(id)=>{
+    const updateTodos=todos.filter(item=>item.id!==id);
+    setTodos(updateTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>
+      <h1>Wishlist Application</h1>
+      <input type="text" value={todo} onChange={onToInputChange} placeholder="Add item to wishlist" />
+      <button onClick={addTodo}>Add</button>
+      </div>
+      <div>
+        <h2>Your Wishlist:</h2>
+          {todos?.length>0 && todos.map((item)=>(
+            <div key={item.id}>
+            <label>
+              <input type='checkbox' onClick={()=>onTodoCheckChange(item.id)}/>
+              <span>{item.todo}</span>
+              <button onClick={()=>deleteTodo(item.id)}>Delete</button>
+            </label>
+            </div>
+          ))}
+      </div>
+      
+    </>
   );
 }
 
